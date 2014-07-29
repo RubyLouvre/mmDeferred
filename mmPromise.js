5c8b11df26a341b3baaa9158e0706a7004efffeb
司徒正美 <cheng19840218@gmail.com>
2014-07-29 14:03:56 +0800

    Update mmPromise.js

--- mmPromise.js
+++ mmPromise.js
@@ -1,98 +1,181 @@
-var Promise = function (executor) {
-    this._callbacks = [];
-    executor(this._resolve.bind(this), this._reject.bind(this));
-};
-Promise.resolve = function (value) {
-    return new Promise(function (resolve) { resolve(value); });
-};
-Promise.reject = function (error) {
-    return new Promise(function (resolve, reject) { reject(error); });
-};
-Promise.cast = function (value) {
-    if (value instanceof Promise) return value;
-    return Promise.resolve(value);
-};
+define(["avalon"], function(avalon) {
 
-Promise.prototype = {
+    var Promise = function(executor) {
+        this._callbacks = [];
+        var that = this
+        if (typeof this !== 'object')
+            throw new TypeError('Promises must be constructed via new')
+        if (typeof executor !== 'function')
+            throw new TypeError('not a function')
+        executor(function(value) {
+            that._resolve(value)
+        }, function(reason) {
+            that._reject(reason)
+        })
+    }
+    var setImmediate = typeof window.setImmediate === "function" ? function(fn) {
+        window.setImmediate(fn)
+    } : function(fn) {
+        window.setTimeout(fn, 0)
+    }
 
-    // Private properties and methods:
-    _failed: false,
-    _resolved: false,
-    _settled: false,
-    _release: function (onSuccess, onFail) {
-        if (this._failed) {
-            if (typeof onFail === 'function') onFail(this._value);
-            else throw this._value;
-        } else {
-            if (typeof onSuccess === 'function') onSuccess(this._value);
-        }
-    },
-    _resolve: function (value) {
-        if (this._resolved) return;
-        this._resolved = true;
-        if (value instanceof Promise) {
-            value.done(this._settle.bind(this), function (error) {
-                this._failed = true;
-                this._settle(error);
-            }.bind(this));
-        } else {
-            this._settle(value);
-        }
-    },
-    _reject: function (value) {
-        if (this._resolved) return;
-        this._resolved = true;
-        this._failed = true;
-        this._settle(value);
-    },
-    _settle: function (value) {
-        this._settled = true;
-        this._value = value;
-        // Do not release before `resolve` or `reject` returns
-        setTimeout(this._callbacks.forEach.bind(this._callbacks, function (data) {
-            this._release(data.onSuccess, data.onFail);
-        }, this), 0);
-    },
-
-    // Public API:
-    // Warning: Some implementations do not provide `done`
-    done: function (onSuccess, onFail) {
-        if (this._settled) {
-            // Do not release before `done` returns
-            setTimeout(this._release.bind(this, onSuccess, onFail), 0);
-        } else {
-            this._callbacks.push({ onSuccess: onSuccess, onFail: onFail });
-        }
-    },
-    then: function (onSuccess, onFail) {
-        var parent = this;
-        return new Promise(function (resolve, reject) {
-            parent.done(function (value) {
-                if (typeof onSuccess === 'function') {
-                    try {
-                        value = onSuccess(value);
-                    } catch (e) {
-                        reject(e);
-                        return;
-                    }
-                }
-                resolve(value);
-            }, function (value) {
-                if (typeof onFail === 'function') {
-                    try {
-                        value = onFail(value);
-                    } catch (e) {
-                        reject(e);
-                        return;
-                    }
-                    resolve(value);
-                } else {
-                    reject(value);
-                }
-            });
+    Promise.resolve = function(value) {
+        return new Promise(function(resolve) {
+            resolve(value);
         });
-    },
-    catch: function (onFail) {
-        return this.then(null, onFail);
-  }
-};
+    };
+    Promise.reject = function(reason) {
+        return new Promise(function(resolve, reject) {
+            reject(reason);
+        });
+    };
+    function transmit(that, value) {
+        that._fired = true;
+        that._value = value;
+        setImmediate(function() {
+            that._callbacks.forEach(function(data) {
+                that._fire(data.onSuccess, data.onFail);
+            })
+        })
+    }
+    Promise.prototype = {
+        _state: "pending", //判定当前状态
+        _fired: false, //判定是否已经被触发
+        _resolve: function(value) {
+            if (this._state !== "pending")
+                return;
+            this._state = "fulfilled"
+            var that = this
+            if (value instanceof Promise) {
+                value._then(function(val) {
+                    transmit(that, val)
+                }, function(reason) {
+                    that._state = "rejected"
+                    transmit(that, reason)
+                });
+
+            } else 
+                if (value && typeof value.then === "function") {
+                value.then.call(value, function(val) {
+                    transmit(that, val)
+                }, function(reason) {
+                    that._state = "rejected"
+                    transmit(that, reason)
+                })
+            } else {
+                transmit(that, value);
+            }
+        },
+        _reject: function(value) {
+            if (this._state !== "pending")
+                return;
+            this._state = "rejected"
+            transmit(this, value)
+        },
+        _fire: function(onSuccess, onFail) {
+            if (this._state === "rejected") {
+                if (typeof onFail === "function")
+                    onFail(this._value);
+                else
+                    throw this._value;
+            } else {
+                if (typeof onSuccess === "function")
+                    onSuccess(this._value);
+            }
+        },
+        _then: function(onSuccess, onFail) {
+            if (this._fired) {
+                var that = this
+                setTimeout(function() {
+                    that._fire(onSuccess, onFail)
+                }, 0);
+            } else {
+                this._callbacks.push({onSuccess: onSuccess, onFail: onFail});
+            }
+        },
+        then: function(onSuccess, onFail) {
+            var parent = this
+            return new Promise(function(resolve, reject) {
+                parent._then(function(value) {
+                    if (typeof onSuccess === "function") {
+                        try {
+                            value = onSuccess(value);
+                        } catch (e) {
+                            reject(e)
+                            return
+                        }
+                    }
+                    resolve(value)
+                }, function(value) {
+                    if (typeof onFail === "function") {
+                        try {
+                            value = onFail(value)
+                        } catch (e) {
+                            reject(e)
+                            return
+                        }
+                        resolve(value)
+                    } else {
+                        reject(value)
+                    }
+                });
+            });
+        },
+        "catch": function(onFail) {
+            return this.then(null, onFail)
+        },
+        done: function(onSuccess) {
+            return this.then(onSuccess)
+        },
+        fail: function(onFail) {
+            return this.then(null, onFail)
+        }
+    }
+
+    function some(any, promises) {
+        var n = 0, result = [], end
+        return new Promise(function(resolve, reject) {
+            function loop(promise, index) {
+                promise.then(function(ret) {
+                    if (!end) {
+                        result[index] = ret//保证回调的顺序
+                        n++
+                        if (any || n >= promises.length) {
+                            resolve(any ? ret : result)
+                            end = true
+                        }
+                    }
+                }, function(e) {
+                    end = true
+                    reject(e)
+                })
+            }
+            for (var i = 0, l = promises.length; i < l; i++) {
+                loop(promises[i], i)
+            }
+        })
+    }
+
+    Promise.all = function() {
+        return some(false, arguments)
+    }
+    Promise.race = function() {
+        return some(true, arguments)
+    }
+    Promise.isPromise = function(obj) {
+        return !!(obj && typeof obj.then === "function")
+    }
+    var nativePromise = window.Promise
+    if (/native code/.test(window.Promise)) {
+        nativePromise.prototype.done = Promise.prototype.done
+        nativePromise.prototype.fail = Promise.prototype.fail
+        nativePromise.isPromise = Promise.isPromise
+        nativePromise.any = nativePromise.race
+    } else {
+        Promise.any = Promise.race
+        window.Promise = Promise
+    }
+    avalon.mmPromise = Promise
+    return avalon
+})
+//https://github.com/ecomfe/er/blob/master/src/Deferred.js
