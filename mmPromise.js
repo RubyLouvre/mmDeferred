16fc92589d745cb4b43a86e559c280d542101c48
RubyLouvre <cheng19840218@gmail.com>
2015-03-26 11:39:50 +0800

    update

--- mmPromise.js
+++ mmPromise.js
@@ -1,4 +1,4 @@
-define("nativePromise", ["avalon"], function (avalon) {
+define(["avalon"], function (avalon) {
 //chrome36的原生Promise还多了一个defer()静态方法，允许不通过传参就能生成Promise实例，
 //另还多了一个chain(onSuccess, onFail)原型方法，意义不明
 //目前，firefox24, opera19也支持原生Promise(chrome32就支持了，但需要打开开关，自36起直接可用)
